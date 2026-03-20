d91cc7530c25a15b92e6c74355f0aa8a1013ad7c
benjahauge <80514177+benjahauge@users.noreply.github.com>
2026-03-20 14:35:56 +0100

    Containerization (#2)
    * add docker images to release
    
    * add Dockerfile aswell
    
    * add permissions to workflow
    
    * more permissions
    
    * support arm64 linux

--- FileSearchEngine/App/Global.cs
+++ FileSearchEngine/App/Global.cs
@@ -22,18 +22,27 @@ public static class Global
 
     public static string SearchQuery = "";
     public static Config? Config { get; set; }
-    public static List<string> CustomPaths { get; set; } = Config?.CustomPaths ?? [];
-
+    public static List<string> CustomPaths { get; set; } = [];
     
     static Global()
     {
         var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        DefaultSearchPaths =
-        [
-            Path.Combine(home, "Documents"),
-            Path.Combine(home, "Desktop"),
-            Path.Combine(home, "Projects"),
-        ];
+        
+        var envPaths = Environment.GetEnvironmentVariable("SEARCH_PATHS");
+        if (!string.IsNullOrEmpty(envPaths))
+        {
+            CustomPaths = envPaths.Split(':').ToList();
+            DefaultSearchPaths = CustomPaths.ToArray();
+        }
+        else
+        {
+            DefaultSearchPaths =
+            [
+                Path.Combine(home, "Documents"),
+                Path.Combine(home, "Desktop"),
+                Path.Combine(home, "Projects"),
+            ];
+        }
 
         FileIndex = new List<FileResult>();
     }
